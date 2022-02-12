f3ce41154bd6e8a3254ece5ab0399a64a50ea499
cbell-smythe <50560779+cbell-smythe@users.noreply.github.com>
2022-02-12 15:00:02 -0500

    Created component to handle search function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+        <header className="App-header"> 📖 Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Coded by Chrystal Bell-Smythe</footer>
+      </div>
     </div>
   );
 }
