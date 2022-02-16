e15eacb514f040b4a146226d07bcc639942c500a
cbell-smythe <50560779+cbell-smythe@users.noreply.github.com>
2022-02-16 13:32:55 -0500

    Created component to handle the synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -8,11 +9,13 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
+            <div>
               {definition.definition}
               <br />
               <em>{definition.example}</em>
-            </p>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
+            </div>
           </div>
         );
       })}
